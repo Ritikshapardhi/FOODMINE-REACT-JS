4711b4babd3f29cb41408a0895d19cf5c61feb1b
Ritikshapardhi <ritikshapardhi29@gmail.com>
2024-04-12 11:27:49 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
+function App() {
+  return (
+    <>
+  {/* <Loading /> */}
+  <Header />
+  <AppRoutes /> 
+   </>
+  
+  );
+}
+export default App;
